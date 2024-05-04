d1f3f120a2b3a88d8f4c499ef521ac9654e749da
Prabakaran Kannan <prabakaran.kannan1@Admins-MacBook-Pro.local>
2024-05-04 10:59:55 +0530

    Speaker section fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var countDownDate = new Date("May 12, 2024 09:00:00").getTime();
+var countDownDate = new Date("June 06, 2024 09:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
